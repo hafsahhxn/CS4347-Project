ddca4f90fe6f3266ae71fa6c752dd9eedd5df19c
hafsahhxn <153801896+hafsahhxn@users.noreply.github.com>
2025-11-29 13:46:52 -0600

    Update operations.py
    added the check in function and "from datetime import date, timedelta"

--- operations.py
+++ operations.py
@@ -1,5 +1,6 @@
 # operations.py
 import sqlite3
+from datetime import date, timedelta
 from database import create_connection
 
 def search_books(query: str):
@@ -49,7 +50,49 @@ def search_books(query: str):
     return results
 
 # Teammates will implement these
-def checkout_book(isbn, card_id): return "[Pending] Teammate"
+def checkout_book(isbn, card_id): 
+    conn = create_connection()
+    c = conn.cursor()
+#cannot check out if borrower has unpaid files
+    c.execute("""
+              SELECT SUM(Fine_amt)
+              FROM FINES f
+              JOIN BOOK_LOANS bl ON f.Loan_id = bl.Loan_id
+              WHERE bl.Card_id = ? AND Paid = 0
+              """, (card_id,))
+    row = c.fetchone()
+    if row[0] is not None:
+        conn.close()
+        return "Checkout failed: borrower has unpaid fines."
+#borrower can have max 3 active loans 
+    c.execute("""
+        SELECT COUNT(*) FROM BOOK_LOANS
+        WHERE Card_id = ? AND Date_in IS NULL
+    """, (card_id,))
+    count = c.fetchone()[0]
+    if count >= 3:
+        conn.close()
+        return "Checkout failed: borrower already has 3 books."
+#cant check out the book if it is not avaliable 
+    c.execute("""
+        SELECT 1 FROM BOOK_LOANS
+        WHERE Isbn = ? AND Date_in IS NULL
+    """, (isbn,))
+    if c.fetchone():
+        conn.close()
+        return "Checkout failed: book is already checked out."
+#checkout book
+    today = date.today()
+    due_date = today + timedelta(days=14)
+
+    c.execute("""
+        INSERT INTO BOOK_LOANS (Isbn, Card_id, Date_out, Due_date)
+        VALUES (?, ?, ?, ?)
+    """, (isbn, card_id, today, due_date))
+    conn.commit()
+    conn.close()
+    return "Checkout successful!"
+    
 def checkin_book(isbn, card_id): return "[Pending] Teammate"
 def add_borrower(ssn, name, address, phone=None): return "[Pending] Teammate"
 def pay_fines(card_id): return "[Pending] Teammate"
